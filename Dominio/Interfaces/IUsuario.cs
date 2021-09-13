58766cad79af63b86553237b25945790aca45c48
Valdir <valdir.silva@fcamara.com.br>
2021-09-13 00:42:49 -0300

    first commit

---
+++ Dominio/Interfaces/IUsuario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Interfaces
+{
+    public interface IUsuario
+    {
+        Task<bool> AdicionaUsuario(string email, string senha, int idade, string celular);
+    }
+}
---
+++ Infraestrutura/Repositorio/RepositorioUsuario.cs
@@ -0,0 +1,54 @@
+﻿using Dominio.Interfaces;
+using Entidades.Entidades;
+using Infraestrutura.Configuracoes;
+using Infraestrutura.Repositorio.Genericos;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestrutura.Repositorio
+{
+    public class RepositorioUsuario : RepositorioGenerico<ApplicationUser>, IUsuario
+    {
+
+        private readonly DbContextOptions<Contexto> _optionsbuilder;
+
+        public RepositorioUsuario()
+        {
+            _optionsbuilder = new DbContextOptions<Contexto>();
+        }
+
+        public async Task<bool> AdicionaUsuario(string email, string senha, int idade, string celular)
+        {
+
+            try
+            {
+                using (var data = new Contexto(_optionsbuilder))
+                {
+                    await data.ApplicationUser.AddAsync(
+                          new ApplicationUser
+                          {
+                              Email = email,
+                              PasswordHash = senha,
+                              Idade = idade,
+                              Celular = celular
+                          });
+
+                    await data.SaveChangesAsync();
+
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+
+            return true;
+
+        }
+    }
+}
